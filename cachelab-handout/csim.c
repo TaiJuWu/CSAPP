336e7b56aace5a29b881b91e499790688096fc6b
tjwu <tjwu1217@gmail.com>
2023-03-17 10:26:07 +0800

    chore: finish arg parser

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,55 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
-int main()
+struct Cache {
+	uint32_t hit;
+	uint32_t miss;
+	uint32_t eviction;
+};
+
+struct Parameter {
+	uint32_t num_of_set_idx;
+	uint32_t associ;
+	uint32_t num_of_block_bit;
+	char trace_file[64];
+};
+
+static struct Parameter para;
+
+int parse_arg(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
-    return 0;
+	for (int i = 1; i < argc; ++i) {
+		if (!strcmp(argv[i], "-s")) {
+			para.num_of_set_idx = atoi(argv[++i]);
+		} else if (!strcmp(argv[i], "-E")) {
+			para.associ = atoi(argv[++i]);
+		} else if (!strcmp(argv[i], "-b")) {
+			para.num_of_block_bit = atoi(argv[++i]);
+		} else if (!strcmp(argv[i], "-t")) {
+			i++;
+			memcpy(para.trace_file, argv[i], strlen(argv[i]));
+		} else {
+			printf("no such argument %s\n", argv[i]);
+		}
+	}
+	return 0;
+}
+
+void print_arg()
+{
+	printf("associ %d\n", para.associ);
+	printf("para.num_of_block_bit %d\n", para.num_of_block_bit);
+	printf("para.num_of_set_idx %d\n", para.num_of_set_idx);
+	printf("trace file %s\n", para.trace_file);
+}
+
+int main(int argc, char *argv[])
+{
+	// printSummary(0, 0, 0);
+	parse_arg(argc, argv);
+	print_arg();
+	return 0;
 }
